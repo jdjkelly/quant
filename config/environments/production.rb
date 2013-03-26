99c2e903a1ba2d66eda43197beb0d6a088431d40
Joshua Kelly <jdjkelly@gmail.com>
2013-03-26 01:41:37 -0400

    Config hotfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,7 @@ Bodyimage::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => Settings.action_mailer.default_url_options.host }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
