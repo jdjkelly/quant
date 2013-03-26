f2f663e48b3de43b0d93ea1cac2f78b83086fccb
Joshua Kelly <jdjkelly@gmail.com>
2013-03-26 02:10:14 -0400

    Configured development.rb for action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Bodyimage::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => Settings.action_mailer.default_url_options.host }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
