8696ab00ea7d83aa2d236c96951d8f5c9fcb3a74
Joshua Kelly <jdjkelly@gmail.com>
2013-03-30 01:50:11 -0400

    Moved assets precompile config to config/application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 Bodyimage::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.assets.initialize_on_precompile = false
-
   # Code is not reloaded between requests
   config.cache_classes = true
 
