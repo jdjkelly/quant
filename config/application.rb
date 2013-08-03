4f184ae7e97a11670e94588b4de76bb9c9029cde
Joshua Kelly <jdjkelly@gmail.com>
2013-08-03 18:50:40 -0400

    Version bump

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module Bodyimage
   class Application < Rails::Application
-    VERSION = "0.0.10"
+    VERSION = "0.0.11"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
