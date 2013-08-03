a7e3a91a00c505b2fda71efd2f3eeea73acb8408
Joshua Kelly <jdjkelly@gmail.com>
2013-08-03 01:45:28 -0400

    Removed erronoeous attr on Weight

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -19,7 +19,7 @@
 
 class Weight < ActiveRecord::Base
   attr_accessible :value, :recorded_at, :lean_mass_value, :fat_mass_value, :fat_percentage,
-                  :grpid, :source, :meta
+                  :source, :meta
 
   # We use this to store provider-specific metadata about the weight. In the case of withings,
   # we get a pkey called grpid that is useful for maintaining a single copy of each record.
