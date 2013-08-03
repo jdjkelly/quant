faba2eaa62eccd06d743d61f13810ba2bd774894
Joshua Kelly <jdjkelly@gmail.com>
2013-08-03 13:45:27 -0400

    Tidied up home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,8 @@
 class HomeController < ApplicationController
   # GET /home
-  # GET /home.json
   def index
     respond_to do |format|
       format.html # index.html.erb
-      # format.json { render json: @measurements }
     end
   end
 end
