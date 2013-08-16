f0fd54c75a48f5bb5a45c48a49f99e8c46f84a47
Joshua Kelly <jdjkelly@gmail.com>
2013-08-16 00:55:12 -0400

    Fixed bug in Meal serializer

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,4 +1,4 @@
 class MealSerializer < ActiveModel::Serializer
-  attributes :id, :date, :calories, :carbohydrates_percantage, :fat_percentage,
+  attributes :id, :date, :calories, :carbohydrates_percentage, :fat_percentage,
              :protein_percentage
 end
