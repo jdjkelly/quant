0fd7650e1f2d20a064579a90acc717d631d9a0f9
Joshua Kelly <jdjkelly@gmail.com>
2013-08-19 17:29:39 -0400

    Fixed authentication controller

--- app/controllers/token_authentications_controller.rb
+++ app/controllers/token_authentications_controller.rb
@@ -2,16 +2,17 @@
 
 class TokenAuthenticationsController < ApplicationController
   before_filter :authenticate_user!
-  load_and_authorize_resource
 
   def create
     @user = User.find(params[:user_id])
+    authorize! :edit, @user
     @user.reset_authentication_token!
     redirect_to edit_user_registration_path(@user)
   end
 
   def destroy
     @user = User.find(params[:id])
+    authorize! :edit, @user
     @user.authentication_token = nil
     @user.save
     redirect_to edit_user_registration_path(@user)
