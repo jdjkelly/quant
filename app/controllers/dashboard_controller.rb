95912cf28e5643437dc1cffe92ee7a579a926802
Joshua Kelly <jdjkelly@gmail.com>
2013-08-21 00:14:21 -0400

    Hacky way of refreshing all data on dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,10 @@ class DashboardController < ApplicationController
   # GET /dashboard
   # GET /dashboard.json
   def index
+    Thread.new do
+      current_user.sync_all_provider_data
+    end
+
     respond_to do |format|
       format.html # index.html.erb
       format.json
