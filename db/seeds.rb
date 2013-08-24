0922e18b4e9722cc7d2b8b191db7e19c2c2a68ca
Joshua Kelly <jdjkelly@gmail.com>
2013-08-24 12:20:33 -0400

    Fixed dashboard bug and added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.all.each do |user|
+  5.times do |i|
+    user.weights.create(value: rand(200), date: Time.now - i.days, lean_mass: rand(150), source: "withings")
+    user.meals.create(date: Time.now - i.days, calories: rand(2000), carbohydrates: rand(100), fat: rand(100), protein: rand(100))
+    user.places.create(date: Time.now - i.days, lat: rand(99), lng: rand(99))
+  end
+end
