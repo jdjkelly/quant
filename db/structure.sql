16062119b7c9f734c641273084b916d791ea96dd
Joshua Kelly <jdjkelly@gmail.com>
2014-01-08 10:36:53 -0500

    Updated structure.sql from migrations

--- db/structure.sql
+++ db/structure.sql
@@ -143,6 +143,40 @@ ALTER SEQUENCE meals_id_seq OWNED BY meals.id;
 
 
 --
+-- Name: moods; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE moods (
+    id integer NOT NULL,
+    date timestamp without time zone,
+    user_id integer,
+    rating numeric,
+    description text,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+--
+-- Name: moods_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE moods_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: moods_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE moods_id_seq OWNED BY moods.id;
+
+
+--
 -- Name: places; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -323,6 +357,13 @@ ALTER TABLE ONLY meals ALTER COLUMN id SET DEFAULT nextval('meals_id_seq'::regcl
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
+ALTER TABLE ONLY moods ALTER COLUMN id SET DEFAULT nextval('moods_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
 ALTER TABLE ONLY places ALTER COLUMN id SET DEFAULT nextval('places_id_seq'::regclass);
 
 
@@ -369,6 +410,14 @@ ALTER TABLE ONLY places
 
 ALTER TABLE ONLY meals
     ADD CONSTRAINT meals_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: moods_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY moods
+    ADD CONSTRAINT moods_pkey PRIMARY KEY (id);
 
 
 --
@@ -540,3 +589,5 @@ INSERT INTO schema_migrations (version) VALUES ('20130803175510');
 INSERT INTO schema_migrations (version) VALUES ('20130816020401');
 
 INSERT INTO schema_migrations (version) VALUES ('20130819210218');
+
+INSERT INTO schema_migrations (version) VALUES ('20131231024056');
