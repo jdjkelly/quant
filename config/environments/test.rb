a7099926f0c9a4d11dfc62f6c20f059d113cc9a6
Joshua Kelly <jdjkelly@gmail.com>
2014-01-08 10:36:35 -0500

    Disable delivery notification errors in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,6 @@ Quantify::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.raise_delivery_errors = false
 end
