d1564d9743306cbbd3be765125bac167d5d431f3
Joshua Kelly <jdjkelly@gmail.com>
2014-01-10 21:01:11 -0500

    Visual tweaks and cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def nav_link text, link
+    if current_page? link
+      content_tag :li, class: "active" do
+        link_to text, link
+      end
+    else
+      content_tag(:li) do
+        link_to text, link
+      end
+    end
+  end
 end
