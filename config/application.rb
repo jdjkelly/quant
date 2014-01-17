11f640534958b373633d826e6b728264059fc507
Joshua Kelly <jdjkelly@gmail.com>
2014-01-17 00:06:10 -0500

    Added exceptions class

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Quantify
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
