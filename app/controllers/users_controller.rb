4cfd0b8edbb77c04feac4a7d55d1a4dc948ad28d
Joshua Kelly <jdjkelly@gmail.com>
2014-01-17 00:07:57 -0500

    Moved sync to user#show. Fundamentally, I'm unsure how to implement this intelligently. Maybe it really should just be part of the login process? Done in parallel, this could be reasonably efficient. The trouble with this is that Warden's after_authentication only happens once the user actually signs in - but if it's a cookied or tokened request, how do we know?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
+    begin
+      @user.sync_all_provider_data
+      flash[:notice] = "API sources refreshed."
+    rescue Exceptions::ApiError
+      flash[:alert] = "API error - please connected and reconnect."
+    end
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @user }
--- config/initializers/warden_callbacks.rb
+++ config/initializers/warden_callbacks.rb
@@ -1,3 +1,2 @@
 Warden::Manager.after_authentication do |user,auth,opts|
-  user.sync_all_provider_data
 end
