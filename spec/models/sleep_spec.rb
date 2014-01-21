84dd0287168cedbadaaf02fb3a4334bfa4df8cc4
Joshua Kelly <jdjkelly@gmail.com>
2014-01-21 00:36:19 -0500

    Tweaked name of sleep ends after start validator, made private, and added a few matchers to sleep spec

--- spec/models/sleep_spec.rb
+++ spec/models/sleep_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Sleep do
+  it { should validate_presence_of :start }
+  it { should validate_presence_of :end }
+  it { should belong_to(:user) }
+
   it "validates that the end is after the start time" do
     pending
   end
