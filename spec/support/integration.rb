46a4e0e3a4e5a63c480963b6a15b5223746b80b8
Joshua Kelly <jdjkelly@gmail.com>
2014-01-21 00:28:19 -0500

    Syntax change for Ruby hashes

--- spec/support/integration.rb
+++ spec/support/integration.rb
@@ -1,4 +1,4 @@
 RSpec.configure do |config|
-  config.include Integration::ActionMailerHelpers, :type => :request
-  config.include Integration::OmniauthHelpers, :type => :request
+  config.include Integration::ActionMailerHelpers, type: :request
+  config.include Integration::OmniauthHelpers, type: :request
 end
