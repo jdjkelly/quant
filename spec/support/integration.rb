bc30f724e3652c9c26a5a6a2655074e1de97d75a
Joshua Kelly <jdjkelly@gmail.com>
2014-01-20 20:26:16 -0500

    CircleCI bombs on these without requiring them

--- spec/support/integration.rb
+++ spec/support/integration.rb
@@ -1,4 +1,7 @@
+require "#{Rails.root}/spec/support/integration/action_mailer_helpers.rb"
+require "#{Rails.root}/spec/support/integration/omniauth_helpers.rb"
+
 RSpec.configure do |config|
-  config.include Integration::ActionMailerHelpers, :type => :request
-  config.include Integration::OmniauthHelpers, :type => :request
+  config.include Integration::ActionMailerHelpers, type: > :request
+  config.include Integration::OmniauthHelpers, type: => :request
 end
