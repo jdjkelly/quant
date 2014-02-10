8c159939f1f761121e7f0ecfee690c2f07ee7e3a
Joshua Kelly <jdjkelly@gmail.com>
2014-02-09 19:37:35 -0500

    Added specs for omniauth callbacks controller for Foursquare

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -8,13 +8,6 @@ describe OmniauthCallbacksController do
     before do
       request.env["devise.mapping"] = Devise.mappings[:user]
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:withings]
-
-      stub_request(:get, /.*wbsapi.withings.net\/user\?action=getbyuserid.*/).
-        to_return(:status => 200, :body => "{\"status\":0,\"body\":{\"users\":[{\"id\":0,\"firstname\":\"Test\",\"lastname\":\"User\",\"shortname\":\"TES\",\"gender\":0,\"fatmethod\":131,\"birthdate\":598467600,\"ispublic\":5}]}}", :headers => {})
-
-      stub_request(:get, /.*wbsapi.withings.net\/measure\?action=getmeas.*/).
-        to_return(:status => 200, :body => "{\"status\":0,\"body\":{\"updatetime\":1370730432,\"more\":370,\"measuregrps\":[{\"grpid\":123986552,\"attrib\":0,\"date\":1370691572,\"category\":1,\"measures\":[{\"value\":64400,\"type\":1,\"unit\":-3},{\"value\":54519,\"type\":5,\"unit\":-3},{\"value\":15344,\"type\":6,\"unit\":-3},{\"value\":9881,\"type\":8,\"unit\":-3}]}]}}", :headers => {})
-
       WithingsAccount.any_instance.stub(:import).and_return(true)
     end
 
@@ -47,19 +40,6 @@ describe OmniauthCallbacksController do
     before do
       request.env["devise.mapping"] = Devise.mappings[:user]
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:fitbit]
-
-      stub_request(:get, /.*api.fitbit.com\/1\/user\/-\/body\/log\/weight\/date\/.*\/30d.json/).
-        to_return(:status => 200, :body => "{\"weight\":[{\"bmi\":31.45,\"date\":\"2014-01-11\",\"logId\":1389484799000,\"time\":\"23:59:59\",\"weight\":195},{\"bmi\":30.81,\"date\":\"2014-01-18\",\"logId\":1390089599000,\"time\":\"23:59:59\",\"weight\":191}]}", :headers => {})
-
-      stub_request(:get, /.*api.fitbit.com\/1\/user\/-\/sleep\/startTime\/date\/.*\/.*.json/).
-        to_return(status: 200, body: "{\"sleep-startTime\": [{\"dateTime\": \"2014-01-22\",\"value\": \"00:52\"}]}", headers: {})
-
-      stub_request(:get, /.*api.fitbit.com\/1\/user\/-\/sleep\/date\/.*.json/).
-        to_return(status: 200, body: "{\"sleep\":[{\"awakeningsCount\":14,\"duration\":26160000,\"efficiency\":93,\"isMainSleep\":true,\"logId\":98934441,\"minuteData\":[{\"dateTime\":\"00:52:00\",\"value\":\"3\"}],\"minutesAfterWakeup\":0,\"minutesAsleep\":401,\"minutesAwake\":29,\"minutesToFallAsleep\":6,\"startTime\":\"2014-01-22T00:52:00.000\",\"timeInBed\":436}],\"summary\":{\"totalMinutesAsleep\":401,\"totalSleepRecords\":1,\"totalTimeInBed\":436}}")
-
-      stub_request(:get, /.*api.fitbit.com\/1\/user\/-\/profile.json/).
-        to_return(:status => 200, :body => "", :headers => {})
-
       FitbitAccount.any_instance.stub(:import).and_return(true)
     end
 
@@ -90,6 +70,8 @@ describe OmniauthCallbacksController do
 
   describe "GET 'foursquare'" do
     before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:foursquare]
       FoursquareAccount.any_instance.stub(:import).and_return(true)
     end
     context "when a user is signed in" do
@@ -102,17 +84,18 @@ describe OmniauthCallbacksController do
 
     context "when a user already has a FoursquareAccount" do
       it "should set a flash notification" do
-        pending
+        User.any_instance.stub(:has_foursquare_auth?) { true }
+        sign_in user
+        get :foursquare
+        flash[:success].should == "You've already synchronized your Foursquare account"
       end
     end
 
     context "when there is no current user" do
       it "should redirect to login" do
-        pending
+        get :foursquare
+        response.should be_redirect
       end
     end
   end
-
-
-
 end
