5b7d389ff603be84f60ef24478df49b687545261
Joshua Kelly <jdjkelly@gmail.com>
2014-07-12 18:51:33 -0400

    Added model annotations

--- spec/models/journal_entry_spec.rb
+++ spec/models/journal_entry_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: journal_entries
+#
+#  id                   :integer          not null, primary key
+#  feelings             :text
+#  happiness            :integer
+#  strategies           :text
+#  created_at           :datetime
+#  updated_at           :datetime
+#  user_id              :integer
+#  encrypted_happiness  :string(255)
+#  encrypted_strategies :text
+#  encrypted_feelings   :text
+#
+
 require 'spec_helper'
 
 describe JournalEntry do
